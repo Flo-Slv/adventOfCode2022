2d86b615bd01772b4297724e3ee6017751c39af3
Flo Slv <florent3969@hotmail.fr>
2022-12-01 17:52:25 +0100

    🟦 feat: resolve first star

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-node_modules
+require('dotenv').config();
+
+const cookie = process.env.COOKIE;
+
+const func = async () => {
+	const res = await fetch('https://adventofcode.com/2022/day/1/input', {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'text/plain',
+			'Cookie': `session=${cookie}`
+		},
+		credentials: 'include'
+		});
+
+
+	const data = await res.text();
+
+	const val = data.trim().split('\n\n').map(n => n.split('\n'));
+
+	const initial = 0;
+	const result = val.map(n => {
+		return n.reduce((acc, curr) => {
+			return acc + parseInt(curr);
+		}, initial);
+	});
+
+	console.log(Math.max(...result));
+}
+
+func();
