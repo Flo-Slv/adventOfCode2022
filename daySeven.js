601584ad89636d98f94adcf0023a8d7e9703ad8b
Flo Slv <florent3969@hotmail.fr>
2022-12-07 18:20:58 +0100

    🟪 refactor: remove data test

--- daySeven.js
+++ daySeven.js
@@ -4,33 +4,6 @@ const func = async () => {
 	let data = await fetchData('7');
 	data = data.split('\n');
 
-	// Website example
-	// const data = [
-	// 	'$ cd /',
-	// 	'$ ls',
-	// 	'dir a',
-	// 	'14848514 b.txt',
-	// 	'8504156 c.dat',
-	// 	'dir d',
-	// 	'$ cd a',
-	// 	'$ ls',
-	// 	'dir e',
-	// 	'29116 f',
-	// 	'2557 g',
-	// 	'62596 h.lst',
-	// 	'$ cd e',
-	// 	'$ ls',
-	// 	'584 i',
-	// 	'$ cd ..',
-	// 	'$ cd ..',
-	// 	'$ cd d',
-	// 	'$ ls',
-	// 	'4060174 j',
-	// 	'8033020 d.log',
-	// 	'5626152 d.ext',
-	// 	'7214296 k'
-	// ];
-
 	// Parsing
 	const createSystem = data => {
 		// System: name, isDirectory, size, children, parent.
@@ -142,7 +115,5 @@ const func = async () => {
 
 	console.log(partTwo());
 };
-
-
 
 func();
